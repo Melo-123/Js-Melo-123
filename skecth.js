7070e6f92278b6f237d6a2805cf23f564e18506a
Melo-123 <00001070134235sp@al.educacao.sp.gov.br>
2024-05-21 18:41:02 -0300

    adicionando arquivo css e js

---
+++ skecth.js
@@ -0,0 +1,14 @@
+function setup() {
+    createCanvas(600, 400);
+  background("black");}
+  
+  function draw() {
+    
+  
+  stroke ("red");
+  fill ("red");
+  
+    
+    if (mouseIsPressed)
+    {circle (mouseX, mouseY, 15);}
+  }
